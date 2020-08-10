56497b67b47b967f5a6d8ab3c54ca724900ceb8c
David Molina <davidmolina2810@gmail.com>
2020-08-10 16:38:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min = 10000000000000000000 #set min as high int to compare against values of name_hash
   key_of_min = nil #set a nil variable to hold key of min
   if name_hash.length == 0 #if name_hash is empty
-    nil
+    key_of_min
   else #name_hash not empty
     name_hash.each do |key, value| #iterate over key,value pairs
       if value < min #if name_hash[key] < min
