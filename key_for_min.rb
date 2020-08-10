7ad8012089518cf8879cb77053e09ce9abc057e5
David Molina <davidmolina2810@gmail.com>
2020-08-10 16:41:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,6 +10,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value| #iterate over key,value pairs
       if value < min #if name_hash[key] < min
         key_of_min = key #set key_of_min equal to key
+        min = value
       end
     end
   end
